622a34baec2a8b9cde704a9981ffe3db82fe93a2
Quentin Moessner <quentin.moessner@gmail.com>
2025-01-14 10:03:33 +0100

    feat: add Drawer and Dialog components with responsive behavior using vaul-svelte

---
+++ src/lib/components/organisms/Dialog.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import * as Dialog from '$lib/components/ui/dialog/index.js';
+  import type { Snippet } from 'svelte';
+
+  type Props = {
+    open: boolean;
+    title: Snippet;
+    content: Snippet;
+    footer: Snippet;
+  };
+
+  let { open = $bindable<boolean>(), title, content, footer }: Props = $props();
+</script>
+
+<Dialog.Root bind:open>
+  <Dialog.Content class="px-2">
+    <Dialog.Title>{@render title()}</Dialog.Title>
+    {@render content()}
+
+    <Dialog.Footer>
+      {@render footer()}
+    </Dialog.Footer>
+  </Dialog.Content>
+</Dialog.Root>
---
+++ src/lib/components/organisms/Drawer.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import * as Drawer from '$lib/components/ui/drawer/index.js';
+  import type { Snippet } from 'svelte';
+  type Props = {
+    open: boolean;
+    title: Snippet;
+    content: Snippet;
+    footer: Snippet;
+  };
+
+  let { open = $bindable<boolean>(), title, content, footer }: Props = $props();
+</script>
+
+<Drawer.Root bind:open>
+  <Drawer.Content class="px-2">
+    <Drawer.Header>
+      <Drawer.Title>{@render title()}</Drawer.Title>
+    </Drawer.Header>
+    {@render content()}
+    <Drawer.Footer>
+      {@render footer()}
+    </Drawer.Footer>
+  </Drawer.Content>
+</Drawer.Root>
